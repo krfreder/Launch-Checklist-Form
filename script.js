28c6c3057a2a1ef071f2eedfbf67ee638ee49bbe
Kyleah Frederic <krfreder@live.com>
2020-04-10 18:38:45 -0500

    Completed updating shuttle requirements

--- script.js
+++ script.js
@@ -3,43 +3,62 @@ window.addEventListener("load", function() {
 
    launchForm.addEventListener("submit", function() {
       // test code::: alert("submit clicked");
-      validate();
-      updateShuttleRequirements();
+      validateAndUpdate();
    });
 });
 
-function validate() {
+function validateAndUpdate() {
+// update shuttle requirements with user input
+   let updateShuttleRequirements = function() {
+      const launchStatus = document.getElementById("launchStatus");
+      const faultyItems = document.getElementById("faultyItems");
+      const pilotStatus = document.getElementById("pilotStatus");
+      const copilotStatus = document.getElementById("copilotStatus");
+      const fuelStatus = document.getElementById("fuelStatus");
+      const cargoStatus = document.getElementById("cargoStatus");
+      
+      pilotStatus.innerHTML = `${pilotName.value} Ready`;
+      copilotStatus.innerHTML = `${copilotName.value} Ready`;
+   
+      if (parseInt(fuelLevel.value, 10) < 10000) {
+         faultyItems.style.visibility = "visible";
+         fuelStatus.innerHTML = 'Well...looks like you need some more fuel.';
+         launchStatus.innerHTML = 'Shuttle not ready for launch';
+         launchStatus.style.color = "red";
+         event.preventDefault();
+      } else if (parseInt(cargoMass.value, 10) > 10000) {
+         faultyItems.style.visibility = "visible";
+         cargoStatus.innerHTML = 'Well...looks like you put too much stuff on here. Take some off.';
+         launchStatus.innerHTML = 'Shuttle not ready for launch';
+         launchStatus.style.color = "red";
+         event.preventDefault();
+      } else {
+         faultyItems.style.visibility = "visible";
+         launchStatus.innerHTML = "Shuttle is ready for launch";
+         launchStatus.style.color = "green";
+         event.preventDefault();
+      }
+   }
+   
+// validation
    const pilotName = document.getElementById("pilotName");
    const copilotName = document.getElementById("copilotName");
    const fuelLevel = document.getElementById("fuelLevel");
    const cargoMass = document.getElementById("cargoMass");
 
-   // test code::: alert("function was called");
    if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === '') {
       alert("The people at NASA complete their forms. Let's be like the people at NASA and try again.");
       event.preventDefault();
-   }
-   if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) {
+   } else if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) {
       alert("Letters only please and thank you.");
-   }
-   if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+      event.preventDefault();
+   } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
       alert("Oh no, you need a number yo!");
+      event.preventDefault();
+   } else {
+      updateShuttleRequirements();    
    }
 }
-
-function updateShuttleRequirements() {
-   const faultyItems = document.getElementById("faultyItems");
-   const pilotStatus = document.getElementById("pilotStatus");
-   const copilotStatus = document.getElementById("copilotStatus");
-   const fuelStatus = document.getElementById("fuelStatus");
-   const cargoStatus = document.getElementById("cargoStatus");
-
-   pilotStatus.innerHTML = `${pilotName.value} Ready`;
-
-   console.log(faultyItems);
-}
-
-
 
 
 
